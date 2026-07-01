5b12ced9c17911547430831328d10b3827db6dfc
Joaquin Servando Yapura Carrabba <joaquincitoyapura@gmail.com>
2026-06-30 21:07:06 -0300

    Delete fix.js

--- fix.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html');
-// If there's a BOM or weird encoding, reading as buffer and converting to utf8 might show .
-let str = html.toString('utf8');
-str = str.replace(/\?"/g, 'ó');
-str = str.replace(/\?/g, 'ó');
-str = str.replace(//g, 'ó');
-fs.writeFileSync('index.html', str, 'utf8');
-console.log('Fixed index.html encoding!');
